f96a6474c1fea48ddba991d12bb293be5269206a
MattLong34 <mdlong34@gmail.com>
2022-12-13 15:24:57 -0700

    add-styles

---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = ({title}) => {
+  return (
+    <header>
+      <h1>Task Tracker {title}</h1>
+    </header>
+  )
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'gray'
+// }
+
+export default Header
